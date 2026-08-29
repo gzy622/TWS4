33359c853a58e2c44d1b14b04bcafa911e6edeaa
gzy622 <gzy622@users.noreply.github.com>
2026-08-29 22:00:56 +0800

    build: 配置 Release 签名并支持构建安卓 APK

--- js/config/version.js
+++ js/config/version.js
@@ -1,7 +1,7 @@
 (function() {
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
-        time: "2026-08-29 21:18:26",
-        version: "20260829-211826"
+        time: "2026-08-29 22:00:56",
+        version: "20260829-220056"
     };
 })();
