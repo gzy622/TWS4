365192112a2a15622b6a4ad5c74d2b43a90a0e0d
gzy622 <gzy622@users.noreply.github.com>
2026-08-30 21:12:08 +0800

    docs: 规范新增升级诊断工作模式

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 41,
+        versionCode: 42,
         versionName: "1.0.0",
-        time: "2026-08-30 21:12:04",
-        version: "20260830-211204"
+        time: "2026-08-30 21:12:09",
+        version: "20260830-211209"
     };
 })();
