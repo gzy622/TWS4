6d7d7c7d632bdd1279473ad3b1f28437e2d1d991
gzy622 <gzy622@users.noreply.github.com>
2026-08-30 22:23:57 +0800

    build: 构建并同步最新 Release APK 产物

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 43,
+        versionCode: 44,
         versionName: "1.0.0",
-        time: "2026-08-30 21:12:18",
-        version: "20260830-211218"
+        time: "2026-08-30 22:23:57",
+        version: "20260830-222357"
     };
 })();
