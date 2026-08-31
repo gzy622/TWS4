bb5d295e2860e0c575bd2c134c62dc2e58935b54
gzy622 <gzy622@users.noreply.github.com>
2026-08-31 17:15:28 +0800

    feat: 扩展顶部导航栏作业标题可用宽度

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 49,
+        versionCode: 50,
         versionName: "1.0.0",
-        time: "2026-08-31 17:14:04",
-        version: "20260831-171404"
+        time: "2026-08-31 17:15:28",
+        version: "20260831-171528"
     };
 })();
