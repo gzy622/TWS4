5a5959ac5c311eb182670c3572fed70e98c0405c
gzy622 <gzy622@users.noreply.github.com>
2026-08-31 17:38:29 +0800

    refactor: 统一二级全屏界面的组件规范与样式体系

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 54,
+        versionCode: 55,
         versionName: "1.0.0",
-        time: "2026-08-31 17:34:42",
-        version: "20260831-173442"
+        time: "2026-08-31 17:38:29",
+        version: "20260831-173829"
     };
 })();
