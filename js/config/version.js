3bf84a7a88a64ab3320cae8b7486cb6e4e52b6ab
gzy622 <gzy622@users.noreply.github.com>
2026-08-31 17:40:58 +0800

    feat: 配置更纱黑体优先渲染与全局字体族栈

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 55,
+        versionCode: 56,
         versionName: "1.0.0",
-        time: "2026-08-31 17:38:29",
-        version: "20260831-173829"
+        time: "2026-08-31 17:40:58",
+        version: "20260831-174058"
     };
 })();
