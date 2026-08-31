d0635ecded73861561934ce533869bc9307151bc
gzy622 <gzy622@users.noreply.github.com>
2026-08-31 20:50:48 +0800

    style: 优化顶栏标题居中与自适应对齐

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 64,
+        versionCode: 65,
         versionName: "1.0.0",
-        time: "2026-08-31 20:42:39",
-        version: "20260831-204239"
+        time: "2026-08-31 20:50:48",
+        version: "20260831-205048"
     };
 })();
