4dd64b166d22d24fd521b4b8643e2b4cd17d2042
gzy622 <gzy622@users.noreply.github.com>
2026-08-31 22:10:47 +0800

    style: 优化快捷面板「突出显示科目」区域样式与布局

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 73,
+        versionCode: 74,
         versionName: "1.0.0",
-        time: "2026-08-31 22:03:30",
-        version: "20260831-220330"
+        time: "2026-08-31 22:10:48",
+        version: "20260831-221048"
     };
 })();
