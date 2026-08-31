7dd85318431fd21660884640c49444616fed5db8
gzy622 <gzy622@users.noreply.github.com>
2026-08-31 20:54:36 +0800

    style: 增强顶栏新建按钮视觉体量与对称度

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 65,
+        versionCode: 66,
         versionName: "1.0.0",
-        time: "2026-08-31 20:50:48",
-        version: "20260831-205048"
+        time: "2026-08-31 20:54:36",
+        version: "20260831-205436"
     };
 })();
