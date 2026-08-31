2292a2950f6c0f44ef2c639d9c9ab053f96d5542
gzy622 <gzy622@users.noreply.github.com>
2026-08-31 22:12:27 +0800

    fix: 修复快捷面板课表视图科目高亮区被并排布局的问题

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 74,
+        versionCode: 75,
         versionName: "1.0.0",
-        time: "2026-08-31 22:10:48",
-        version: "20260831-221048"
+        time: "2026-08-31 22:12:27",
+        version: "20260831-221227"
     };
 })();
