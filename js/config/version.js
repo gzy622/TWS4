c97ba05641385d07d2af05815e2794bf4e8b9f8e
gzy622 <gzy622@users.noreply.github.com>
2026-09-01 09:39:04 +0800

    style: 统一课程表抽屉条目样式

--- js/config/version.js
+++ js/config/version.js
@@ -2,9 +2,9 @@
     window.TWS3 = window.TWS3 || {};
     window.TWS3.BUILD_INFO = {
         appVersion: "1.0.0",
-        versionCode: 83,
+        versionCode: 84,
         versionName: "1.0.0",
-        time: "2026-09-01 09:35:39",
-        version: "20260901-093539"
+        time: "2026-09-01 09:39:04",
+        version: "20260901-093904"
     };
 })();
